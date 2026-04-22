a5b14a89098cd87fa6854e9a59a934b52b9a97b5
Sergej Goroxov <sergej.goroxov.07@mail.ru>
2026-04-22 14:16:30 +0300

    refactor: move database's configs to separate file

---
+++ src/db/config.py
@@ -0,0 +1,17 @@
+# КОНФИГИ
+db_config_pg = {
+    "host": "127.0.0.1",
+    "port": 5432,
+    "user": "user",
+    "password": "1234",
+    "dbname": "mybd"
+}
+
+db_config_mysql = {
+    "host": "localhost",
+    "user": "admin",
+    "password": "admin",
+    "database": "new_schema",
+    "use_pure": True,
+    "port": 3306
+}

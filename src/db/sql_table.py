dcde44a24bbb18650185b693249e9253c65d9ac3
Sergej Goroxov <sergej.goroxov.07@mail.ru>
2026-04-22 14:16:30 +0300

    refactor: move database's configs to separate file

--- src/db/sql_table.py
+++ src/db/sql_table.py
@@ -266,24 +266,6 @@ class SQLTable:
         self.connection.close()
 
 
-# КОНФИГИ
-db_config_pg = {
-    "host": "127.0.0.1",
-    "port": 5432,
-    "user": "user",
-    "password": "1234",
-    "dbname": "mybd"
-}
-
-db_config_mysql = {
-    "host": "localhost",
-    "user": "admin",
-    "password": "admin",
-    "database": "new_schema",
-    "use_pure": True,
-    "port": 3306
-}
-
 """
 фичи:
   добавлен lazy import
